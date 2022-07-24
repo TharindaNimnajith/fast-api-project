5e4865a730030d9891723e487ac0afe50b50eba8
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-07-24 20:24:43 +0530

    init

--- restClient/com/intellij/httpClient/http/request/run/rhino/js-rhino-response-handler.js
+++ restClient/com/intellij/httpClient/http/request/run/rhino/js-rhino-response-handler.js
@@ -1,145 +1,140 @@
 function ContentType(mimeType, charset) {
-  this.mimeType = mimeType
-  this.charset = charset
+    this.mimeType = mimeType
+    this.charset = charset
 }
 
 function Variables(global) {
-  this.vars = {}
-
-  for (var key in global) {
-    this.vars[global[key].getKey()] = global[key].getValue()
-  }
-
-  this.set = function (varName, varValue) {
-    this.vars[varName] = varValue
-  }
-
-  this.get = function (varName) {
-    return this.vars[varName]
-  }
-
-  this.isEmpty = function () {
-    return Object.keys(this.vars).length == 0
-  }
-
-  this.clear = function (varName) {
-    delete this.vars[varName]
-  }
-
-  this.clearAll = function () {
     this.vars = {}
-  }
+
+    for (var key in global) {
+        this.vars[global[key].getKey()] = global[key].getValue()
+    }
+
+    this.set = function (varName, varValue) {
+        this.vars[varName] = varValue
+    }
+
+    this.get = function (varName) {
+        return this.vars[varName]
+    }
+
+    this.isEmpty = function () {
+        return Object.keys(this.vars).length == 0
+    }
+
+    this.clear = function (varName) {
+        delete this.vars[varName]
+    }
+
+    this.clearAll = function () {
+        this.vars = {}
+    }
 }
 
 function HttpClient(vars, canelable, logPrinter) {
-  this.tests = {}
-  this.global = new Variables(vars)
+    this.tests = {}
+    this.global = new Variables(vars)
 
-  this.test = function (testName, func) {
-    this.tests[testName] = func || null
-  }
-
-  this.assert = function (condition, message) {
-    if (!condition) {
-      throw message || "Assert failed"
+    this.test = function (testName, func) {
+        this.tests[testName] = func || null
     }
-  }
 
-  this.log = function (text) {
-    logPrinter.accept(text + "\n")
-  }
+    this.assert = function (condition, message) {
+        if (!condition) {
+            throw message || "Assert failed"
+        }
+    }
 
-  this.exit = function () {
-    canelable.accept(this)
-  }
+    this.log = function (text) {
+        logPrinter.accept(text + "\n")
+    }
+
+    this.exit = function () {
+        canelable.accept(this)
+    }
 }
 
 function ResponseHeaders(headers) {
-  this.headers = headers
+    this.headers = headers
 
-  this.valueOf = function (headerName) {
-    var len = this.headers.length
-    for (var i = 0; i < len; i++) {
-      if (headerName == this.headers[i].name) {
-        return this.headers[i].value
-      }
+    this.valueOf = function (headerName) {
+        var len = this.headers.length
+        for (var i = 0; i < len; i++) {
+            if (headerName == this.headers[i].name) {
+                return this.headers[i].value
+            }
+        }
     }
-  }
 
-  this.valuesOf = function (headerName) {
-    var values = []
-    var len = this.headers.length
-    for (var i = 0; i < len; i++) {
-      if (headerName == headers[i].name) {
-        values.push(headers[i].value)
-      }
+    this.valuesOf = function (headerName) {
+        var values = []
+        var len = this.headers.length
+        for (var i = 0; i < len; i++) {
+            if (headerName == headers[i].name) {
+                values.push(headers[i].value)
+            }
+        }
+        return values
     }
-    return values
-  }
 }
 
 function LineStreamResponse(subscriberConsumer, isJson) {
-  this.onEachLine = function (subscriber, onFinish) {
-    const finalSubscriber = isJson
-      ? function (line, unsubscribe) {
-        var newLine
-        try {
-          newLine = JSON.parse(line)
-        }
-        catch (e) {
-          newLine = line
-        }
-        subscriber(newLine, unsubscribe)
-      }
-      : subscriber;
-    subscriberConsumer.accept(finalSubscriber, onFinish)
-  }
+    this.onEachLine = function (subscriber, onFinish) {
+        const finalSubscriber = isJson
+            ? function (line, unsubscribe) {
+                var newLine
+                try {
+                    newLine = JSON.parse(line)
+                } catch (e) {
+                    newLine = line
+                }
+                subscriber(newLine, unsubscribe)
+            }
+            : subscriber;
+        subscriberConsumer.accept(finalSubscriber, onFinish)
+    }
 }
 
 var jsHandler = function (
-  isJson,
-  global,
-  responseBody,
-  statusCode,
-  mimeType,
-  charset,
-  headers,
-  cancelable,
-  logPrinter,
-  subscriberConsumer,
-  isStreaming
+    isJson,
+    global,
+    responseBody,
+    statusCode,
+    mimeType,
+    charset,
+    headers,
+    cancelable,
+    logPrinter,
+    subscriberConsumer,
+    isStreaming
 ) {
-  var client = new HttpClient(global, cancelable, logPrinter)
+    var client = new HttpClient(global, cancelable, logPrinter)
 
-  var userVisibleBody
-  try {
-    if (isStreaming) {
-      userVisibleBody = new LineStreamResponse(subscriberConsumer, isJson)
+    var userVisibleBody
+    try {
+        if (isStreaming) {
+            userVisibleBody = new LineStreamResponse(subscriberConsumer, isJson)
+        } else {
+            userVisibleBody = isJson ? JSON.parse(responseBody) : responseBody
+        }
+    } catch (e) {
+        userVisibleBody = responseBody
     }
-    else {
-      userVisibleBody = isJson ? JSON.parse(responseBody) : responseBody
-    }
-  }
-  catch (e) {
-    userVisibleBody = responseBody
-  }
 
-  var response = {
-    body: userVisibleBody,
-    status: statusCode,
-    headers: new ResponseHeaders(headers),
-    contentType: new ContentType(mimeType, charset)
-  }
-  try {
-    //<CUSTOM_SCRIPT_PLACEHOLDER>
-    return client
-  }
-  catch (e) {
-    if (e instanceof JavaException && e.message.startsWith("com.intellij.httpClient.http.request.run.rhino.HttpClientRhinoResponseHandler$ExitScriptException")) {
-      return client
+    var response = {
+        body: userVisibleBody,
+        status: statusCode,
+        headers: new ResponseHeaders(headers),
+        contentType: new ContentType(mimeType, charset)
     }
-    else {
-      throw e
+    try {
+        //<CUSTOM_SCRIPT_PLACEHOLDER>
+        return client
+    } catch (e) {
+        if (e instanceof JavaException && e.message.startsWith("com.intellij.httpClient.http.request.run.rhino.HttpClientRhinoResponseHandler$ExitScriptException")) {
+            return client
+        } else {
+            throw e
+        }
     }
-  }
 }
--- restClient/com/intellij/ws/rest/client/stubs/http-client.d.ts
+++ restClient/com/intellij/ws/rest/client/stubs/http-client.d.ts
@@ -20,134 +20,134 @@ declare const response: HttpResponse;
  * values are not preserved after IDE restart.
  */
 interface HttpClient {
-  /**
-   * Global variables defined in response handler scripts,
-   * can be used as variables in HTTP Requests,
-   *
-   * Example:
-   * ### Authorization request, receives token as an attribute of json body
-   * GET https://example.com/auth
-   *
-   * > {% client.global.set("auth_token", response.body.token) %}
-   *
-   * ### Request executed with received auth_token
-   * GET http://example.com/get
-   * Authorization: Bearer {{auth_token}}
-   */
-  global: Variables;
+    /**
+     * Global variables defined in response handler scripts,
+     * can be used as variables in HTTP Requests,
+     *
+     * Example:
+     * ### Authorization request, receives token as an attribute of json body
+     * GET https://example.com/auth
+     *
+     * > {% client.global.set("auth_token", response.body.token) %}
+     *
+     * ### Request executed with received auth_token
+     * GET http://example.com/get
+     * Authorization: Bearer {{auth_token}}
+     */
+    global: Variables;
 
-  /**
-   * Creates test with name 'testName' and body 'func'.
-   * All tests will be executed right after response handler script.
-   */
-  test (testName: string, func: Function): void;
+    /**
+     * Creates test with name 'testName' and body 'func'.
+     * All tests will be executed right after response handler script.
+     */
+    test(testName: string, func: Function): void;
 
-  /**
-   * Checks that condition is true and throw an exception otherwise.
-   * @param condition
-   * @param message if specified it will be used as an exception message.
-   */
-  assert(condition: boolean, message?: string): void;
+    /**
+     * Checks that condition is true and throw an exception otherwise.
+     * @param condition
+     * @param message if specified it will be used as an exception message.
+     */
+    assert(condition: boolean, message?: string): void;
 
-  /**
-   * Prints text to the response handler or test stdout and then terminates the line.
-   */
-  log(text: string): void;
+    /**
+     * Prints text to the response handler or test stdout and then terminates the line.
+     */
+    log(text: string): void;
 
-  exit(): void;
+    exit(): void;
 }
 
 /**
  * Variables storage, can be used to define, undefine or retrieve variables.
  */
 interface Variables {
-  /**
-   * Saves variable with name 'varName' and sets its value to 'varValue'.
-   */
-  set(varName: string, varValue: string): void;
+    /**
+     * Saves variable with name 'varName' and sets its value to 'varValue'.
+     */
+    set(varName: string, varValue: string): void;
 
-  /**
-   * Returns value of variable 'varName'.
-   */
-  get(varName: string): string;
+    /**
+     * Returns value of variable 'varName'.
+     */
+    get(varName: string): string;
 
-  /**
-   * Checks no variables are defined.
-   */
-  isEmpty(): boolean;
+    /**
+     * Checks no variables are defined.
+     */
+    isEmpty(): boolean;
 
-  /**
-   * Removes variable 'varName'.
-   * @param varName {string}
-   */
-  clear(varName: string): void;
+    /**
+     * Removes variable 'varName'.
+     * @param varName {string}
+     */
+    clear(varName: string): void;
 
-  /**
-   * Removes all variables.
-   */
-  clearAll(): void;
+    /**
+     * Removes all variables.
+     */
+    clearAll(): void;
 }
 
 /**
  * Represents response as stream of lines. In case of JSON stream, lines will be objects
  */
 interface LineStreamResponse {
-  onEachLine(subscriber: (line: string | object, unsubscribe: () => void) => void, onFinish?: () => void): void;
+    onEachLine(subscriber: (line: string | object, unsubscribe: () => void) => void, onFinish?: () => void): void;
 }
 
 /**
  * HTTP Response data object, contains information about response content, headers, status, etc.
  */
 interface HttpResponse {
-  /**
-   * Response content, it is a string or JSON object if response content-type is json.
-   */
-  body: string | LineStreamResponse | object;
+    /**
+     * Response content, it is a string or JSON object if response content-type is json.
+     */
+    body: string | LineStreamResponse | object;
 
-  /**
-   * Response headers storage.
-   */
-  headers: ResponseHeaders;
+    /**
+     * Response headers storage.
+     */
+    headers: ResponseHeaders;
 
-  /**
-   * Response status, e.g. 200, 404, etc.
-   */
-  status: number;
+    /**
+     * Response status, e.g. 200, 404, etc.
+     */
+    status: number;
 
-  /**
-   * Value of 'Content-Type' response header.
-   */
-  contentType: ContentType;
+    /**
+     * Value of 'Content-Type' response header.
+     */
+    contentType: ContentType;
 }
 
 /**
  * Headers storage, can be use to retrieve data about header value.
  */
 interface ResponseHeaders {
-  /**
-   * Retrieves the first value of 'headerName' response header or null otherwise.
-   */
-  valueOf(headerName: string): string | null;
+    /**
+     * Retrieves the first value of 'headerName' response header or null otherwise.
+     */
+    valueOf(headerName: string): string | null;
 
-  /**
-   * Retrieves all values of 'headerName' response header. Returns empty list if header with 'headerName' doesn't exist.
-   */
-  valuesOf(headerName: string): string[];
+    /**
+     * Retrieves all values of 'headerName' response header. Returns empty list if header with 'headerName' doesn't exist.
+     */
+    valuesOf(headerName: string): string[];
 }
 
 /**
  * Content type data object, contains information from 'Content-Type' response header.
  */
 interface ContentType {
-  /**
-   * MIME type of the response,
-   * e.g. 'text/plain', 'text/xml', 'application/json'.
-   */
-  mimeType: string;
+    /**
+     * MIME type of the response,
+     * e.g. 'text/plain', 'text/xml', 'application/json'.
+     */
+    mimeType: string;
 
-  /**
-   * String representation of the response charset,
-   * e.g. utf-8.
-   */
-  charset: string;
+    /**
+     * String representation of the response charset,
+     * e.g. utf-8.
+     */
+    charset: string;
 }
